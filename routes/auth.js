1d3c6fb21c750a42d778475936963082658b2184
Andres Huanca Namuche <andreshuanca@upeu.edu.pe>
2023-02-09 17:18:11 -0500

    add middleware auth

--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,7 @@ const router = Router();
 //post - validaciones- Login
 router.post('/login',[
     check('email', 'El email no es valido').isEmail(),
+    check('email', 'El email es obligatorio').not().isEmpty(),
     check( 'password', 'La contraseña es obligatoria' ).notEmpty(),
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
